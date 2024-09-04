934c941b162c812ab252941b13fcf0f126df22e7
DavidPortillo26 <anicetod03@gmail.com>
2024-09-04 16:28:09 -0500

    revert

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-""" plots y as a line graph """
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-line_plot = plt.plot(y, 'r-')
-
-# Save the figure
-plt.savefig('0-line.png')
-
-# Close the plot
-plt.close()
+plt.plot(y, 'r-')
+plt.axis([0, 10, None, None])
+plt.show()
