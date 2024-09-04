a38c1a1ff5b99f71eadbaa0a69e1f7f7df98259e
DavidPortillo26 <anicetod03@gmail.com>
2024-09-04 11:35:50 -0500

    file 4 complete

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+""" function to add two arrays elements-wise"""
 
-add_arrays = __import__('4-line_up').add_arrays
 
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
-print(add_arrays(arr1, [1, 2, 3]))
+def add_arrays(arr1, arr2):
+    """ adding two arrays element wise
+
+    Args:
+        arr1, arr2: Given arrays
+
+    Return:
+        the sum of arrays: new matrix
+
+    """
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        return [sum(x) for x in zip(arr1, arr2)]
