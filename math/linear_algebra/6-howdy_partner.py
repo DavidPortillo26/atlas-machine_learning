3922b9332c916133af9a938b44ae5d22d952b538
DavidPortillo26 <anicetod03@gmail.com>
2024-09-04 12:43:32 -0500

    file 6 complete

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+"""function to concatenate 2 arrays into a new one"""
 
-cat_arrays = __import__('6-howdy_partner').cat_arrays
 
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [6, 7, 8]
-print(cat_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
+def cat_arrays(arr1, arr2):
+    """ concatenate two arrays into a new one
+
+    Args:
+        arr1, arr2: Given array
+
+    Return:
+        the new list of arrays: arr3
+
+    """
+    arr3 = []
+    arr3 = arr1 + arr2
+    return arr3
