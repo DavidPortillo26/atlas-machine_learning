eb0905b1de8d00cf97d352dc4b952ba1ddcf2b01
DavidPortillo26 <anicetod03@gmail.com>
2024-09-04 15:19:51 -0500

    File 12 complete

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
+"""function for basic operations of matrices using numpy"""
 
-import numpy as np
-np_elementwise = __import__('12-bracin_the_elements').np_elementwise
 
-mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-mat2 = np.array([[1, 2, 3], [4, 5, 6]])
+def np_elementwise(mat1, mat2):
+    """ operate matrices
 
-print(mat1)
-print(mat2)
-add, sub, mul, div = np_elementwise(mat1, mat2)
-print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
-add, sub, mul, div = np_elementwise(mat1, 2)
-print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
+    Args:
+        mat1, mat2: Given matrix
+
+    Return:
+        matrix operation
+    """
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
