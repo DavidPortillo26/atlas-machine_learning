2cb3dff1c847d258d757f1fe05c7fc65e65d232d
DavidPortillo26 <anicetod03@gmail.com>
2024-09-16 12:02:15 -0500

    ws

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -8,11 +8,11 @@ class Neuron:
         "Check if nx is an integer"
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        
+    
         "Check if nx is a positive integer"
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
+    
         "Initialize the weights (W), bias (b), and activated output (A)"
         self.W = np.random.randn(1, nx)
         self.b = 0
