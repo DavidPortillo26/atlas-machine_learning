df733d089372a3b20b485c527a62e85364a000e9
DavidPortillo26 <anicetod03@gmail.com>
2024-09-16 12:15:10 -0500

    rewriting documentation

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
+"""
+defines Neuron class that defines
+a single neuron performing binary classification
+"""
+
+
 import numpy as np
 
 
 class Neuron:
-    def __init__(self, nx):
-        """Initialize the neuron"""
-        """Check if nx is an integer"""
-        if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+    """
+    class that represents a single neuron performing binary classification
 
-        """Check if nx is a positive integer"""
+    class constructor:
+        def __init__(self, nx)
+
+    public instance attributes:
+        W: the weights vector for the neuron
+        b: the bias for the neuron
+        A: the activated output of the neuron (prediction)
+    """
+
+    def __init__(self, nx):
+        """
+        class constructor
+
+        parameters:
+            nx [int]: the number of input features to the neuron
+            If nx is not an integer, raise a TypeError.
+            If nx is less than 1, raise a ValueError.
+
+        sets public instance attributes:
+            W: the weights vector for the neuron,
+                initialized using a random normal distribution
+            b: the bias for the neuron,
+                initialized to 0
+            A: the activated output of the neuron (prediction),
+                initialized to 0
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
-        """Initialize the weights (W), bias (b), and activated output (A)"""
-        """Random normal distibution to weight"""
         self.W = np.random.randn(1, nx)
-        """Bias initialized to 0"""
         self.b = 0
-        """Activated output initialized to 0"""
         self.A = 0
