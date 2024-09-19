8840ac4c0781242a9f1a26eed8cf69d9a4c70871
DavidPortillo26 <anicetod03@gmail.com>
2024-09-18 21:21:44 -0500

    file 25 rewrite

--- supervised_learning/classification/25-one_hot_encode.py
+++ supervised_learning/classification/25-one_hot_encode.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 """
-defines function that converts a one-hot matrix
+converts a one-hot matrix
 into a vector of labels
 """
-
-
 import numpy as np
 
 
 def one_hot_decode(one_hot):
-    """
-    converts a one-hot matrix into a numeric vector of labels
-
-    parameters:
-        one-hot [numpy.ndarray with shape (classes, m)]:
-            one-hot encoded matrix to decode
-            classes: the maximum number of classes
-            m: the number of examples
-    returns:
-        numpy.ndarray with shape (m,) containing the numeric labels,
-            or None if fails
-    """
-    if type(one_hot) is not np.ndarray or len(one_hot.shape) != 2:
+    """ doc """
+    if type(one_hot) is not np.ndarray\
+       or len(one_hot.shape) != 2:
         return None
-    vector = one_hot.transpose().argmax(axis=1)
-    return vector
+    """arr = []
+    for i in one_hot.T:
+        new = np.where(i == 1)[0]
+        if len(new) != 0:
+            arr.append(new[0])
+    return np.array(arr)"""
+    try:
+        return np.argmax(one_hot, axis=0)
+    except Exception:
+        return None
