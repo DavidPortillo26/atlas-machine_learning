8b0edca3a8950be6d9388a60139a04fc08f7e0b0
DavidPortillo26 <anicetod03@gmail.com>
2024-10-01 10:47:35 -0500

    file 1 completed

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Tensorflow project
+"""
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+    """ Returns tensor output of the layer """
+    w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(n, activation=activation,
+                            kernel_initializer=w, name='layer')
+    return layer(prev)
