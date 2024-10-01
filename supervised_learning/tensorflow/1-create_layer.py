63ed741104ba4e3520f0aa8bccef433f2ace696c
DavidPortillo26 <anicetod03@gmail.com>
2024-10-01 10:50:10 -0500

    attempt to fix layer problem

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
-"""
-    Tensorflow project
-"""
+"""Script to create a layer"""
+
 import tensorflow.compat.v1 as tf
 
 
 def create_layer(prev, n, activation):
-    """ Returns tensor output of the layer """
-    w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(n, activation=activation,
-                            kernel_initializer=w, name='layer')
+    """
+    method to create a TF layer
+    Args:
+        prev: tensor of the previous layer
+        n: n nodes created
+        activation: activation function
+
+    Returns: Layer created with shape n
+
+    """
+    # Average number of inputs and output connections.
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode='FAN_AVG')
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=initializer,
+                            name='layer')
     return layer(prev)
