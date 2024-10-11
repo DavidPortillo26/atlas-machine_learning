edd7820ff7b9f1390f116c9962ad668a92c19287
DavidPortillo26 <anicetod03@gmail.com>
2024-10-11 18:07:57 -0500

    file 3 third times the charm

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,67 +1,32 @@
-#!/usr/bin/env python3
-""" train using mini-batch gradient descent"""
-import tensorflow as tf
+import numpy as np
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
-
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid, b_size=32, epochs=5,
-                     load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
-    """ train using mini-batch gradient descent
-    @X_train: numpy.ndarray of shape (m, 784) containing training data
-        @m: number of data points
-        @784: number of input features
-    @Y_train: is a one-hot numpy.ndarray shape (m, 10) to training labels
-        @10: number of classes model should classify
-    @X_valid: numpy.ndarray of shape (m, 784) containing validation data
-    @Y_valid: is one-hot numpy.ndarray shape (m, 10) validation labels
-    @b_size: is the number of data points in a batch
-    @epochs: number of times training should pass thorugh dataset
-    @load_path: path to where the model should be loaded
-    @save_path: path to where the model should be saved
-    Returns: the path where the model was saved
+def create_mini_batches(X, Y, batch_size):
     """
-
-    with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(load_path + '.meta')
-        saver.restore(sess, load_path)
-
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        loss = tf.get_collection('loss')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        train_op = tf.get_collection('train_op')[0]
-
-        size = X_train.shape[0]
-        inner = size // b_size if size % b_size == 0 else size // b_size + 1
-
-        for i in range(epochs + 1):
-            # shuffle to avoid over fitting
-            X_s, Y_s = shuffle_data(X_train, Y_train)
-
-            t_cost = sess.run(loss, feed_dict={x: X_train, y: Y_train})
-            t_acc = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            v_cost = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
-            v_acc = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
-
-            print('After {} epochs:'.format(i))
-            print('\tTraining Cost: {}'.format(t_cost))
-            print('\tTraining Accuracy: {}'.format(t_acc))
-            print('\tValidation Cost: {}'.format(v_cost))
-            print('\tValidation Accuracy: {}'.format(v_acc))
-
-            if i < epochs:
-                end = b_size
-                for j in range(inner):
-                    start = j * b_size
-                    end = (j + 1) * b_size if end <= size else size
-                    feed = {x: X_s[start: end], y: Y_s[start: end]}
-                    sess.run(train_op, feed_dict=feed)
-                    if (j + 1) % 100 == 0 and j != 0:
-                        c_cost = sess.run(loss, feed_dict=feed)
-                        c_acc = sess.run(accuracy, feed_dict=feed)
-                        print("\tStep {}:".format(j + 1))
-                        print("\t\tCost: {}".format(c_cost))
-                        print("\t\tAccuracy: {}".format(c_acc))
-
-        save_path = saver.save(sess, save_path)
-    return save_path
+    Creates mini-batches for mini-batch gradient descent.
+    
+    Args:
+        X: numpy.ndarray of shape (m, nx), input data
+           m is the number of data points
+           nx is the number of features in X
+        Y: numpy.ndarray of shape (m, ny), labels
+           m is the same number of data points as in X
+           ny is the number of classes (or targets) for classification tasks
+        batch_size: number of data points per batch
+    
+    Returns:
+        List of mini-batches, where each mini-batch is a tuple (X_batch, Y_batch)
+    """
+    # Shuffle the data
+    X, Y = shuffle_data(X, Y)
+    
+    m = X.shape[0]  # Number of data points
+    mini_batches = []
+    
+    # Create mini-batches
+    for i in range(0, m, batch_size):
+        X_batch = X[i:i + batch_size]
+        Y_batch = Y[i:i + batch_size]
+        mini_batches.append((X_batch, Y_batch))
+    
+    return mini_batches
