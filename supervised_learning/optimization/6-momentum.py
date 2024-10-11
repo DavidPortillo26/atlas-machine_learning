092a98332d3bc0b24d80c3c1e5e1212cf1c776b3
DavidPortillo26 <anicetod03@gmail.com>
2024-10-11 18:21:31 -0500

    layout

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Set up gradient descent with momentum optimization algorithm in TensorFlow."""
+"""Set up gradient descent with momentum
+optimization algorithm in TensorFlow."""
 import tensorflow as tf
 
 
@@ -12,9 +13,10 @@ def create_momentum_op(alpha, beta1):
         beta1: Momentum weight (scalar).
 
     Returns:
-        A TensorFlow optimizer object configured for momentum optimization.
+        A TensorFlow optimizer object configured
+        for momentum optimization.
     """
     # Create the optimizer using Stochastic Gradient Descent (SGD) with momentum
     optimizer = tf.optimizers.SGD(learning_rate=alpha, momentum=beta1)
-    
+
     return optimizer
