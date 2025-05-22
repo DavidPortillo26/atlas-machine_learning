ace730e3060f9e9375452db9d7da46427a28e73f
DavidPortillo26 <anicetod03@gmail.com>
2025-05-21 22:11:45 -0500

    edited file rewrite

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,37 +1,59 @@
 #!/usr/bin/env python3
-""" performs K-means on a dataset """
+""" E-step: expected value of likelihood function
+    respect the conditional distribution
+"""
 
 import numpy as np
-
 pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
-    """ doc """
-    if type(X) is not np.ndarray or X.ndim != 2\
-       or type(pi) is not np.ndarray or pi.ndim != 1\
-       or type(m) is not np.ndarray or m.ndim != 2\
-       or type(S) is not np.ndarray or S.ndim != 3:
+    """
+    Function that calculates the expectation step in the EM algorithm for a GMM
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the data set
+        pi: numpy.ndarray of shape (k,) containing the priors for each cluster
+        m: numpy.ndarray of shape (k, d) containing the centroid means for each
+           cluster
+        S: numpy.ndarray of shape (k, d, d) containing the covariance matrices
+           for each cluster
+    Returns: g, l, or None, None on failure
+             g: numpy.ndarray of shape (k, n) containing the posterior
+                probabilities for each data point in each cluster
+             l: is the total log likelihood
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    try:
-        n, d = X.shape
-        k = pi.shape[0]
-        k1, d1 = m.shape
-        k2, d2, d3 = S.shape
-        if (k != k1 or k != k2 or k1 != k2)\
-           or (d != d1 or d != d2 or d != d3)\
-           or (d1 != d2 or d1 != d3 or d2 != d3)\
-           or np.any(np.linalg.det(S)) == 0\
-           or not np.isclose(pi.sum(), 1):
-            return None, None
-        pdfs = np.ndarray((k, n))
-        for i in range(k):
-            pdfs[i] = pdf(X, m[i], S[i])
-        pdfs = pdfs * pi[:, np.newaxis]
-        pdfsum = pdfs.sum(axis=0)
-        # posterior probabilities for each data point in each cluster
-        g = pdfs / pdfsum
-        total_log_likelihood = np.log(pdfsum).sum()
-        return g, total_log_likelihood
-    except Exception:
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
         return None, None
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+    if not np.isclose([np.sum(pi)], [1])[0]:
+        return None, None
+
+    n, d = X.shape
+    k = pi.shape[0]
+    if d != m.shape[1] or d != S.shape[1] or d != S.shape[2]:
+        return None, None
+    if k != m.shape[0] or k != S.shape[0]:
+        return None, None
+
+    centroids_mean = m
+    covariance_mat = S
+    gauss_components = np.zeros((k, n))
+
+    # http://ethen8181.github.io/machine-learning/clustering/GMM/GMM.html
+    # P(j) P(Xn | j) / ∑k j=1 P(j) P(Xn | j)
+    for i in range(k):
+        likelihood = pdf(X, centroids_mean[i], covariance_mat[i])
+        prior = pi[i]
+        gauss_components[i] = likelihood * prior
+    g = gauss_components / np.sum(gauss_components, axis=0)
+
+    # https://zhiyzuo.github.io/EM/
+    # Log likelihood: ∑i ln ∑z^i p((x^i) , (z^i);Θ)
+    log_likelihood = np.sum(np.log(np.sum(gauss_components, axis=0)))
+
+    return g, log_likelihood
