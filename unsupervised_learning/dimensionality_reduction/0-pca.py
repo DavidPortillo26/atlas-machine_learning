db8fed8a3a9b02a2c24f3c8d9eca90c2cd032df4
DavidPortillo26 <anicetod03@gmail.com>
2025-05-21 21:23:16 -0500

    trying to find were the ws is needed

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -8,8 +8,8 @@ def pca(X, var=0.95):
     _, s, vh = np.linalg.svd(X)
     n = 0
     s_var = s.sum() * var
-    tot = s[0] 
-    while(tot < s_var):
+    tot = s[0]
+    while(tot < s_var): 
         n += 1
         tot += s[n]
     return vh[:n+1].T
