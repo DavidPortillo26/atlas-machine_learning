786834be43f7091d2089cf9d2c70ca53f28ee3b3
DavidPortillo26 <anicetod03@gmail.com>
2025-05-22 11:52:08 -0500

    pycodestyle

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -6,26 +6,26 @@ Determinant function
 
 def multi_determinant(matrix):
     """
-    function that computes the determinant of a given matrix of
-    +3D
+    Function that computes the determinant of a given matrix of
+    dimension >= 2.
+
     Args:
         matrix: list of lists whose determinant should be calculated
-    Returns: Determinant of matrix
+
+    Returns:
+        Determinant of matrix
     """
     mat_l = len(matrix)
     if mat_l == 2 and len(matrix[0]) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+        return (matrix[0][0] * matrix[1][1] -
+                matrix[1][0] * matrix[0][1])
 
     deter = 0
-    cols = list(range(len(matrix)))
-    for c in cols:
-        mat_cp = [r[:] for r in matrix]
-        mat_cp = mat_cp[1:]
-        rows = range(len(mat_cp))
-
-        for r in rows:
-            mat_cp[r] = mat_cp[r][0:c] + mat_cp[r][c + 1:]
-        sign = (-1) ** (c % 2)
+    for c in range(mat_l):
+        mat_cp = [row[:] for row in matrix[1:]]
+        for r in range(len(mat_cp)):
+            mat_cp[r] = mat_cp[r][:c] + mat_cp[r][c + 1:]
+        sign = (-1) ** c
         sub_det = multi_determinant(mat_cp)
         deter += sign * matrix[0][c] * sub_det
     return deter
@@ -33,23 +33,20 @@ def multi_determinant(matrix):
 
 def determinant(matrix):
     """
-    calculates the determinant of a matrix
+    Calculates the determinant of a matrix.
+
     Args:
         matrix: list of lists whose determinant should be calculated
-    Returns: Determinant of matrix
+
+    Returns:
+        Determinant of matrix
     """
     mat_l = len(matrix)
-    if type(matrix) != list or len(matrix) == 0:
+    if not isinstance(matrix, list) or mat_l == 0:
         raise TypeError("matrix must be a list of lists")
-    if not all([type(mat) == list for mat in matrix]):
+    if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-    if matrix[0] and mat_l != len(matrix[0]):
-        raise ValueError("matrix must be a square matrix")
     if matrix == [[]]:
         return 1
-    if mat_l == 1 and len(matrix[0]) == 1:
-        return matrix[0][0]
-    if not all(mat_l == len(col) for col in matrix):
-        raise ValueError("matrix must be a square matrix")
-
-    return multi_determinant(matrix)
+    if not all(mat_l == len(row) for row in matrix):
+        raise
