e60441737a97d764558c91831effc7c45a9a7d3a
DavidPortillo26 <anicetod03@gmail.com>
2025-05-22 11:20:35 -0500

    file rewrite edit

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,96 +1,44 @@
 #!/usr/bin/env python3
-""" Bayesian Probability """
+"""Intersection"""
+
 import numpy as np
-import scipy.special as special
-
-
-def likelihood(x, n, P):
-    """
-    ****************************************************
-    ****** calculates the likelihood of obtaining ******
-    *this data given various hypothetical probabilities*
-    *********of developing severe side effects**********
-    ****************************************************
-    @x: is the number of patients that develop severe side effects
-    @n: is the total number of patients observed
-    @P: is a 1D numpy.ndarray containing the various hypothetical
-        probabilities of developing severe side effects
-    *** If n is not a positive integer, raise a ValueError with
-        the message n must be a positive integer
-    *** If x is not an integer that is greater than or equal to 0,
-        raise a ValueError with the message x must be an integer
-        that is greater than or equal to 0
-    *** If x is greater than n, raise a ValueError with the message
-        x cannot be greater than n
-    *** If P is not a 1D numpy.ndarray, raise a TypeError with
-        the message P must be a 1D numpy.ndarray
-    *** If any value in P is not in the range [0, 1], raise a ValueError
-        with the message All values in P must be in the range [0, 1]
-    Returns:
-            a 1D numpy.ndarray containing the likelihood of obtaining
-            the data, x and n, for each probability in P, respectively
-    """
-    if type(n) is not int or n <= 0:
-        raise ValueError("n must be a positive integer")
-    if type(x) is not int or x < 0:
-        raise ValueError("x must be an integer that "
-                         "is greater than or equal to 0")
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray or len(P.shape) != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    if np.where(P > 1, 1, 0).any() or np.where(P < 0, 1, 0).any():
-        raise ValueError("All values in P must be in the range [0, 1]")
-    return special.binom(n, x) * pow(P, x) * pow(1 - P, n - x)
 
 
 def intersection(x, n, P, Pr):
     """
-    *********************************************************************
-    ***Calculate intersection of data given hypothetical probabilities***
-    *********************************************************************
-    @x: is the number of patients that develop severe side effects
-    @n: is the total number of patients observed
-    @P: is a 1D numpy.ndarray containing the various hypothetical
-        probabilities of developing severe side effects
-    @Pr: is a 1D numpy.ndarray containing the prior beliefs of P
-    *** If n is not a positive integer, raise a ValueError with
-        the message n must be a positive integer
-    *** If x is not an integer that is greater than or equal to 0,
-        raise a ValueError with the message x must be an integer
-        that is greater than or equal to 0
-    *** If x is greater than n, raise a ValueError with the message
-        x cannot be greater than n
-    *** If P is not a 1D numpy.ndarray, raise a TypeError with
-        the message P must be a 1D numpy.ndarray
-    *** If Pr is not a numpy.ndarray with the same shape as P,
-        raise a TypeError with the message Pr must be a numpy.ndarray
-        with the same shape as P
-    *** If any value in P or Pr is not in the range [0, 1], raise
-        a ValueError with the message All values in {P} must be in
-        the range [0, 1] where {P} is the incorrect variable
-    *** If Pr does not sum to 1, raise a ValueError with the message
-        Pr must sum to 1 Hint: use numpy.isclose
-    *** All exceptions should be raised in the above order
-    Returns:
-            a 1D numpy.ndarray containing the intersection of obtaining
-            x and n with each probability in P, respectively
+    Intersection function
+    Args:
+        x: the number of patients that develop severe side effects
+        n: the total number of patients observed
+        P: 1D numpy.ndarray containing the various hypothetical probabilities
+           of developing severe side effects
+        Pr: 1D numpy.ndarray containing the prior beliefs of P
+    Returns: 1D numpy.ndarray containing the intersection of obtaining x and n
+             with each probability in P, respectively
     """
-    if type(n) is not int or n <= 0:
-        raise ValueError("n must be a positive integer")
-    if type(x) is not int or x < 0:
-        raise ValueError("x must be an integer that is "
-                         "greater than or equal to 0")
+    if not isinstance(n, int) or (n <= 0):
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or (x < 0):
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
     if x > n:
-        raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray or len(P.shape) != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    if type(Pr) is not np.ndarray or P.shape != Pr.shape:
-        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if np.where(P > 1, 1, 0).any() or np.where(P < 0, 1, 0).any():
-        raise ValueError("All values in P must be in the range [0, 1]")
-    if np.where(Pr > 1, 1, 0).any() or np.where(Pr < 0, 1, 0).any():
-        raise ValueError("All values in Pr must be in the range [0, 1]")
-    if not np.isclose(Pr.sum(), 1):
-        raise ValueError("Pr must sum to 1")
-    return likelihood(x, n, P) * Pr
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    if not isinstance(Pr, np.ndarray) or (P.shape != Pr.shape):
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P")
+    if not np.all((Pr >= 0) & (Pr <= 1)):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+    sum = np.sum(Pr)
+    if not np.isclose(sum, 1):
+        raise ValueError('Pr must sum to 1')
+
+    numerator = np.math.factorial(n)
+    denominator = (np.math.factorial(x) * (np.math.factorial(n - x)))
+    factorial = numerator / denominator
+    P_likelihood = factorial * (np.power(P, x)) * (np.power((1 - P), (n - x)))
+    intersection = P_likelihood * Pr
+    return intersection
