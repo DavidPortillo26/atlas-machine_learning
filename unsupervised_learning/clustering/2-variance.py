a2820d13e8327b8813b1eac232c2487d7161a58b
DavidPortillo26 <anicetod03@gmail.com>
2025-05-21 22:06:33 -0500

    file 2

---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" performs K-means on a dataset """
+
+import numpy as np
+
+
+def variance(X, C):
+    """ doc """
+    if type(X) is not np.ndarray or X.ndim != 2\
+       or type(C) is not np.ndarray or C.ndim != 2:
+        return None
+
+    try:
+        sub = np.apply_along_axis(np.subtract, 1, X, C)
+        return (np.linalg.norm(sub, axis=2)**2).min(axis=1).sum()
+        # return ((sub)**2).sum(axis=2).min(axis=1).sum()
+    except Exception:
+        return None
