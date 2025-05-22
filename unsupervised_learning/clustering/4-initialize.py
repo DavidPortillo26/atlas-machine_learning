09305435c842d85715e17d601e4100f60c610fda
DavidPortillo26 <anicetod03@gmail.com>
2025-05-21 22:09:09 -0500

    file 4

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" performs K-means on a dataset """
+
+import numpy as np
+
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """ doc """
+    if type(X) is not np.ndarray or X.ndim != 2\
+       or type(k) is not int or k < 1:
+        return None, None, None
+
+    n, d = X.shape
+    pi = np.full((k,), 1/k)
+    m, _ = kmeans(X, k)
+    S = np.full((k, d, d), np.identity(d))
+    return pi, m, S
