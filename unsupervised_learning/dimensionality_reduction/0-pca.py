56ac230ecac45f9296f02514db6c5666dbcad527
DavidPortillo26 <anicetod03@gmail.com>
2025-05-21 21:22:36 -0500

    ws

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -2,12 +2,13 @@
 """ PCA """
 import numpy as np
 
+
 def pca(X, var=0.95):
     """ performs PCA on a dataset """
     _, s, vh = np.linalg.svd(X)
     n = 0
     s_var = s.sum() * var
-    tot = s[0]
+    tot = s[0] 
     while(tot < s_var):
         n += 1
         tot += s[n]
