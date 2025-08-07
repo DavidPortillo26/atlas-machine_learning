7b744bf6c7bed97886ef4682e2d3aac58bb30745
DavidPortillo26 <anicetod03@gmail.com>
2025-08-07 10:00:30 -0500

    fixed the vectorizer (i hope) by adding out to the end

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -17,21 +17,6 @@ def bag_of_words(sentences, vocab=None):
                 f is the number of features analyzed
             features list of the features used for embeddings
     """
-    # vocab = ['are', 'awesome', 'beautiful', 'cake']
     Vectorizer = text.CountVectorizer(vocabulary=vocab)
     embeddings = Vectorizer.fit_transform(sentences)
-    return embeddings.toarray(), Vectorizer.get_feature_names()
-    """ doing from scratch not necessary
-    features = set()
-    embeddings = [[0]]
-    for sentence in sentences:
-        # new = set(s.replace("'s","").strip('@!#$%^&*()\'\" ').\
-                lower() for s in sentence.split())
-        # print(new)
-        for n in new:
-            features.add(n)
-        #print(features)
-    features = list(features)
-    features.sort()
-    return embeddings, features
-    """
+    return embeddings.toarray(), Vectorizer.get_feature_names_out().tolist()
