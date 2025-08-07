1eeded8f330142f66075e43b413b474eb810686f
DavidPortillo26 <anicetod03@gmail.com>
2025-08-06 22:27:42 -0500

    feat: enhance bag of words function with detailed docstring and updated feature extraction

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -17,7 +17,35 @@ def bag_of_words(sentences, vocab=None):
 
         vocab [list]:
             list of vocabulary words to use for analysis
+            if None, all words within sentences should be usedfrom sklearn.feature_extraction.text import CountVectorizer
+
+
+def bag_of_words(sentences, vocab=None):
+    """
+    Creates a bag of words embedding matrix
+
+    parameters:
+        sentences [list]:
+            list of sentences to analyze
+
+        vocab [list]:
+            list of vocabulary words to use for analysis
             if None, all words within sentences should be used
+
+    returns:
+        embeddings,features:
+            embeddings [numpy.ndarray of shape (s, f)]:
+                contains the embeddings
+                s: number of sentences in sentences
+                f: number of features analyzed
+            features [list]:
+                list of features used for embeddings
+    """
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X_train_counts = vectorizer.fit_transform(sentences)
+    embeddings = X_train_counts.toarray()
+    features = vectorizer.get_feature_names_out()  # Updated method
+    return embeddings, features
 
     returns:
         embeddings,features:
