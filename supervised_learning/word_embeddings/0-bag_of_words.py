bf8d13f7fcc8e3061838150fa7a9d35e209fbca4
DavidPortillo26 <anicetod03@gmail.com>
2025-08-06 22:29:05 -0500

    file rewrite

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,63 +1,37 @@
 #!/usr/bin/env python3
 """
-Defines function that creates a bag of words embedding matrix
+    NLP - Word Embeddings
 """
-
-
-from sklearn.feature_extraction.text import CountVectorizer
-
-
-def bag_of_words(sentences, vocab=None):
-    """
-    Creates a bag of words embedding matrix
-
-    parameters:
-        sentences [list]:
-            list of sentences to analyze
-
-        vocab [list]:
-            list of vocabulary words to use for analysis
-            if None, all words within sentences should be usedfrom sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from sklearn.feature_extraction import text
 
 
 def bag_of_words(sentences, vocab=None):
+    """ Creates a bag of words embedding matrix
+        sentences is a list of sentences to analyze
+        vocab is a list of the vocabulary words to use for the analysis
+            If None, all words within sentences should be used
+        Returns: embeddings, features
+            embeddings ndarray (s, f) containing the embeddings
+                s is the number of sentences in sentences
+                f is the number of features analyzed
+            features list of the features used for embeddings
     """
-    Creates a bag of words embedding matrix
-
-    parameters:
-        sentences [list]:
-            list of sentences to analyze
-
-        vocab [list]:
-            list of vocabulary words to use for analysis
-            if None, all words within sentences should be used
-
-    returns:
-        embeddings,features:
-            embeddings [numpy.ndarray of shape (s, f)]:
-                contains the embeddings
-                s: number of sentences in sentences
-                f: number of features analyzed
-            features [list]:
-                list of features used for embeddings
-    """
-    vectorizer = CountVectorizer(vocabulary=vocab)
-    X_train_counts = vectorizer.fit_transform(sentences)
-    embeddings = X_train_counts.toarray()
-    features = vectorizer.get_feature_names_out()  # Updated method
+    # vocab = ['are', 'awesome', 'beautiful', 'cake']
+    Vectorizer = text.CountVectorizer(vocabulary=vocab)
+    embeddings = Vectorizer.fit_transform(sentences)
+    return embeddings.toarray(), Vectorizer.get_feature_names()
+    """ doing from scratch not necessary
+    features = set()
+    embeddings = [[0]]
+    for sentence in sentences:
+        # new = set(s.replace("'s","").strip('@!#$%^&*()\'\" ').\
+                lower() for s in sentence.split())
+        # print(new)
+        for n in new:
+            features.add(n)
+        #print(features)
+    features = list(features)
+    features.sort()
     return embeddings, features
-
-    returns:
-        embeddings,features:
-            embeddings [numpy.ndarray of shape (s, f)]:
-                contains the embeddings
-                s: number of sentences in sentences
-                f: number of features analyzed
-            features [list]:
-                list of features used for embeddings
     """
-    vectorizer = CountVectorizer(vocabulary=vocab)
-    X_train_counts = vectorizer.fit_transform(sentences)
-    embeddings = X_train_counts.toarray()
-    features = vectorizer.get_feature_names()
-    return embeddings, features
