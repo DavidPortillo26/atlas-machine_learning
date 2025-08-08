d56222b49c0e1cce7d66eb0c2923ec7e506a044e
DavidPortillo26 <anicetod03@gmail.com>
2025-08-07 23:32:56 -0500

    rewrite

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,27 +1,46 @@
+
 #!/usr/bin/env python3
+"""Word Embeddings module"""
 import gensim
+
 
 def word2vec_model(
     sentences,
-    vector_size=100,
+    size=100,
     min_count=5,
     window=5,
     negative=5,
     cbow=True,
-    epochs=5,
+    iterations=5,
     seed=0,
     workers=1
 ):
-    sg = 0 if cbow else 1  # CBOW=0, Skipgram=1
-    model = gensim.models.Word2Vec(
-        sentences=sentences,
-        vector_size=vector_size,
+    """Creates and trains a Word2Vec model
+
+    Arguments:
+        sentences {list[str]} -- Is a list of strings
+
+    Keyword Arguments:
+        size {int} -- Dimensinality of embedding layer (default: {100})
+        min_count {int} -- Minimum number of occurences (default: {5})
+        window {int} -- Maximum distance (default: {5})
+        negative {int} -- Is the size of negative sampling (default: {5})
+        cbow {bool} -- Indicates CBOW or Skip-gram (default: {True})
+        iterations {int} -- Is the number of iterations (default: {5})
+        seed {int} -- Is the seed for the random number (default: {0})
+        workers {int} -- Is the number of workers (default: {1})
+
+    Returns:
+        model -- Trained model
+    """
+    return Word2Vec(
+        sentences,
+        size=size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=sg,
+        iter=iterations,
+        workers=workers,
         seed=seed,
-        workers=workers
+        sg=not cbow
     )
-    
-    return model
