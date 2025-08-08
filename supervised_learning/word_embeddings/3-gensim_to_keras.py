507aa6a0c72f0b22442681128800eb4770a1a3d4
DavidPortillo26 <anicetod03@gmail.com>
2025-08-07 23:10:29 -0500

    contiueation

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
+#!/usr/bin/env python3
 def gensim_to_keras(model):
     """
     Converts a trained gensim Word2Vec model into a Keras Embedding layer.
-
     Args:
         model: Trained gensim Word2Vec model.
-
     Returns:
         A trainable tf.keras.layers.Embedding layer initialized with gensim weights.
     """
     vocab_size = len(model.wv)
     embedding_dim = model.vector_size
     weights = model.wv.vectors  # numpy array
-
     embedding_layer = tf.keras.layers.Embedding(
         input_dim=vocab_size,
         output_dim=embedding_dim,
