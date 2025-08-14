af0154de7e77fb07cc651848a0125683a0fcb616
DavidPortillo26 <anicetod03@gmail.com>
2025-08-14 11:34:55 -0500

    github code

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,32 +1,47 @@
+
 #!/usr/bin/env python3
-import tensorflow_datasets as tfds
+"""
+    Transformer Applications
+"""
 import transformers
+import tensorflow_datasets as tfds
 
-class Dataset:
+
+class Dataset():
+    """ loads and preps a dataset for machine translation """
+
     def __init__(self):
-        # Load the train and validation splits deterministically
-        self.data_train, self.data_valid = tfds.load(
-            "ted_hrlr_translate/pt_to_en",
-            split=["train", "validation"],
-            as_supervised=True,
-            shuffle_files=False
-        )
+        """ creates the instance attributes:
+            data_train, which contains the ted_hrlr_translate/pt_to
+                en tf.data.Dataset train split, loaded as_supervided
+            data_valid, which contains the ted_hrlr_translate/pt
+                to_en tf.data.Dataset validate split, loaded as_supervided
+            tokenizer_pt is the Portuguese tokenizer created
+                from the training set
+            tokenizer_en is the English tokenizer created
+                from the training set
+        """
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.\
+            tokenize_dataset(self.data_train)
 
-        # Build tokenizers
-        self.tokenizer_pt, self.tokenizer_en = self.tokenizer_dataset(self.data_train)
-
-    def tokenizer_dataset(self, data):
-        # Limit to first 100 examples
-        corpus = list(data.take(100).as_numpy_iterator())
-
-        pt_corpus = [pt.decode("utf-8") for pt, _ in corpus]
-        en_corpus = [en.decode("utf-8") for _, en in corpus]
-
-        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            pt_corpus, target_vocab_size=2**15
-        )
-        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            en_corpus, target_vocab_size=2**15
-        )
-
+    def tokenize_dataset(self, data):
+        """ creates sub-word tokenizers for our dataset:
+            data is a tf.data.Dataset whose examples are formatted as a tuple
+                (pt, en)
+            pt is the tf.Tensor containing the Portuguese sentence
+            en is the tf.Tensor containing the corresponding English sentence
+            The maximum vocab size should be set to 2**15
+            Returns: tokenizer_pt, tokenizer_en
+                tokenizer_pt is the Portuguese tokenizer
+                tokenizer_en is the English tokenizer
+        """
+        STE = tfds.deprecated.text.SubwordTextEncoder
+        tokenizer_pt = STE.build_from_corpus(
+            (pt.numpy() for pt, en in data), target_vocab_size=2**15)
+        tokenizer_en = STE.build_from_corpus(
+            (en.numpy() for pt, en in data), target_vocab_size=2**15)
         return tokenizer_pt, tokenizer_en
