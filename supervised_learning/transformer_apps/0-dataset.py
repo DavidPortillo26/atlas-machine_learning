6aeaa17370703a8aa32b81849174e97edb9c00bc
DavidPortillo26 <anicetod03@gmail.com>
2025-08-14 10:53:01 -0500

    edited decode function

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -20,25 +20,23 @@ class Dataset:
         # Build tokenizers from the training dataset
         self.tokenizer_pt, self.tokenizer_en = self.tokenizer_dataset(self.data_train)
 
-    def tokenizer_dataset(self, data, max_examples=5000):
+    def tokenizer_dataset(self, data):
         """
-        Creates SubwordTextEncoder tokenizers for Portuguese and English
-        using only a limited number of examples to avoid timeout.
+        Creates SubwordTextEncoder tokenizers for Portuguese and English.
         """
-        def limited_corpus(lang_index):
-            count = 0
-            for pt, en in data:
-                text = pt.numpy().decode('utf-8') if lang_index == 0 else en.numpy().decode('utf-8')
-                yield text
-                count += 1
-                if count >= max_examples:
-                    break
+        # Materialize dataset so we can iterate multiple times
+        corpus = list(data.as_numpy_iterator())
 
+        # Decode tensors to strings
+        pt_corpus = (pt.decode('utf-8') for pt, en in corpus)
+        en_corpus = (en.decode('utf-8') for pt, en in corpus)
+
+        # Build subword tokenizers
         tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            limited_corpus(0), target_vocab_size=2**13
+            pt_corpus, target_vocab_size=2**13
         )
         tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-            limited_corpus(1), target_vocab_size=2**13
+            en_corpus, target_vocab_size=2**13
         )
 
         return tokenizer_pt, tokenizer_en
