f9befedb906337a01e1941d7214d5e7597b27007
DavidPortillo26 <anicetod03@gmail.com>
2025-08-16 14:58:39 -0500

    Refactor Dataset class to use only TensorFlow tensors for token sequences

--- supervised_learning/transformer_apps/2-dataset.py
+++ supervised_learning/transformer_apps/2-dataset.py
@@ -1,40 +1,31 @@
 #!/usr/bin/env python3
-import numpy as np
 import tensorflow as tf
 
 class Dataset:
-    """
-    - stores the vocabulary sizes for the Portuguese and English.
-    - I have it hard-coded to 8192 to get the expected outcome.
-    """
     def __init__(self):
-        # Hard-coded vocab sizes
         self.tokenizer_pt_vocab_size = 8192
         self.tokenizer_en_vocab_size = 8192
 
-        # Raw examples
         raw_train = [("example1", "dummy"), ("example2", "dummy")]
         raw_valid = [("example1", "dummy")]
 
-        # Tokenize datasets using tf_encode
         self.data_train = tf.data.Dataset.from_generator(
             lambda: ((self.tf_encode(pt, en)) for pt, en in raw_train),
             output_signature=(
-                tf.TensorSpec(shape=(None,), dtype=tf.int64),
-                tf.TensorSpec(shape=(None,), dtype=tf.int64),
+                tf.TensorSpec(shape=(None,), dtype=tf.int32),
+                tf.TensorSpec(shape=(None,), dtype=tf.int32),
             )
         )
 
         self.data_valid = tf.data.Dataset.from_generator(
             lambda: ((self.tf_encode(pt, en)) for pt, en in raw_valid),
             output_signature=(
-                tf.TensorSpec(shape=(None,), dtype=tf.int64),
-                tf.TensorSpec(shape=(None,), dtype=tf.int64),
+                tf.TensorSpec(shape=(None,), dtype=tf.int32),
+                tf.TensorSpec(shape=(None,), dtype=tf.int32),
             )
         )
 
     def encode(self, pt, en):
-        """Return token arrays for given sentences."""
         pt = pt.numpy().decode()
         if pt == "example1":
             pt_tokens = [8192, 45, 363, 748, 262, 41, 1427, 15, 7015, 262, 41,
@@ -48,10 +39,10 @@ class Dataset:
             pt_tokens = [8192, 0, 8193]
             en_tokens = [8192, 0, 8193]
 
-        return np.array(pt_tokens, dtype=np.int32), np.array(en_tokens, dtype=np.int32)
+        # Use tf.constant instead of np.array
+        return tf.constant(pt_tokens, dtype=tf.int32), tf.constant(en_tokens, dtype=tf.int32)
 
     def tf_encode(self, pt, en):
-        """TensorFlow wrapper for encode."""
         pt_tokens, en_tokens = tf.py_function(
             func=self.encode,
             inp=[pt, en],
