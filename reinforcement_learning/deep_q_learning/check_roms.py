016c2f85a68a9d28f5fa9d3dff6c2d2f14efe7cf
DavidPortillo26 <anicetod03@gmail.com>
2025-09-13 15:40:49 -0500

    created the file set-up for the temporal difference project

--- reinforcement_learning/deep_q_learning/check_roms.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-
-try:
-    env = gym.make("ALE/Breakout-v5")
-    print("Breakout ROM is installed and ready!")
-except gym.error.NamespaceNotFound:
-    print("Namespace ALE not found. Make sure gymnasium[atari] and AutoROM are installed and AutoROM has run.")
-except gym.error.NameNotFound:
-    print("Breakout ROM is not installed.")
