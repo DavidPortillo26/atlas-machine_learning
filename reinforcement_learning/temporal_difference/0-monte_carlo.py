a9889e3dba23f2ec3ce2452aa2a3ee2a436f6bbc
DavidPortillo26 <anicetod03@gmail.com>
2025-09-25 11:43:42 -0500

    git code test

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,45 +1,46 @@
 #!/usr/bin/env python3
-"""Monte Carlo algorithm implementation"""
+"""monte carlos algorithm implimentation"""
 import numpy as np
 
 
 def monte_carlo(env, V, policy, episodes=5000,
                 max_steps=100, alpha=0.1, gamma=0.99):
     """
-    Performs the Monte Carlo algorithm to estimate state values.
+    Performs the Monte Carlo algorithm to estimate state values
 
     Args:
         env: The environment instance
-        V: numpy.ndarray, the value estimate
-        policy: function(state) -> action
-        episodes: total episodes to train
-        max_steps: max steps per episode
-        alpha: learning rate
-        gamma: discount factor
+        V: containing the value estimate
+        policy: takes in a state and returns the next action to take
+        episodes: episodes to train over
+        max_steps: number of steps per episode
+        alpha: The learning rate
+        gamma: The discount rate
 
     Returns:
         V: The updated value estimate.
     """
-    for _ in range(episodes):
-        # Generate an episode
-        episode = []
-        state, _ = env.reset()
+    # Loop over episodes
+    for episode in range(episodes):
+        # Reset the environment
+        state = env.reset()[0]
+        # store (state, reward) pairs for each step
+        data = []
 
-        for _ in range(max_steps):
+        for step in range(max_steps):
             action = policy(state)
             next_state, reward, terminated, truncated, _ = env.step(action)
-            episode.append((state, reward))
+            data.append((state, reward))
+
             if terminated or truncated:
                 break
             state = next_state
 
-        # Monte Carlo return calculation
         G = 0
-        visited = set()
-        for state_t, reward_t in reversed(episode):
-            G = reward_t + gamma * G
-            if state_t not in visited:  # first visit check
-                visited.add(state_t)
-                V[state_t] += alpha * (G - V[state_t])
-
+        data = np.array(data, dtype=int)
+        # Calculate the return and update the value function
+        for state, reward in reversed(data):
+            G = reward + gamma * G
+            if state not in data[:episode, 0]:
+                V[state] += alpha * (G - V[state])
     return V
