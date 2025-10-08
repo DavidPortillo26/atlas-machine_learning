6cf6d653fb5de69944d0dcb2f5a9dc4936e2b47a
DavidPortillo26 <anicetod03@gmail.com>
2025-10-07 20:45:24 -0500

    file 1-crop created

---
+++ pipeline/data_augmentation/1-crop.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module that performs a random crop on an image using TensorFlow."""
+
+import tensorflow as tf
+
+
+def crop_image(image, size):
+    """
+    Performs a random crop on an image.
+
+    Args:
+        image: A 3D tf.Tensor representing the image to crop.
+        size: A tuple (height, width, channels) representing the size of the crop.
+
+    Returns:
+        The randomly cropped image (tf.Tensor).
+    """
+    return tf.image.random_crop(image, size)
