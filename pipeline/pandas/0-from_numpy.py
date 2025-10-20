574a86c89f943e58ce278d20c830ccf08d2295d3
DavidPortillo26 <anicetod03@gmail.com>
2025-10-20 15:32:52 -0500

    created 0-from_numpy.py added function to convert NumPy array to pandas DataFrame with labeled columns

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def from_numpy(array):
+    """
+    Create a pandas DataFrame from a NumPy array.
+
+    Parameters
+    ----------
+    array : numpy.ndarray
+        A 2D Numpy arrat where rows are observations and columns are features.
+
+    Returns
+    -------
+    pd.DataFrame
+        A Dataframe with column lables 'A', 'B', 'C', ... in order.
+
+    Notes
+    -----
+    Assumes there are at most 26 columns (A - Z).
+    """
+
+
+    if getattr(array, "ndim", None) != 2:
+        raise ValueError("from_numpy expects a 2D np.mdarray (rows, cols).")
+
+    # Gets number of columns
+    n_cols = array.shape[1]
+    if n_cols > 26:
+        raise ValueError(" This function supports at most 26 columns (A -Z).")
+
+    # Creates column labels
+    columns = [chr(65 + i ) for i in range(n_cols)]
+    return pd.DataFrame(array, columns=columns)
