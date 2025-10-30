ccf45fe3eedb75a727740b1100617c93bd319fe5
DavidPortillo26 <anicetod03@gmail.com>
2025-10-29 22:05:29 -0500

    now defines the users table with id auto-increment primary key, unique non-null email, and optional name, while avoiding failure if the table already exists

---
+++ pipeline/databases/13-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create unique users table with auto increment id
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
